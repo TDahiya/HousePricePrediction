08e6c97c210ad21b7208802d679816c37001cd78
Tanishq Dahiya <tanishqd423@gmail.com>
2024-10-11 19:06:35 +0530

    Modified app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import joblib
 
 app = Flask(__name__)
 
-# Load the best model and scaler
+
 model = joblib.load(open('best_model.pkl', 'rb'))
 scaler = joblib.load(open('scaler.pkl', 'rb'))
 
@@ -15,19 +15,19 @@ def index():
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
-        # Get inputs from the form
+        
         area = float(request.form['area'])
         bhk = float(request.form['bhk'])
         bathroom = float(request.form['bathroom'])
         price_per_sqft = area / float(request.form['price'])  # Assuming price is passed to calculate price per sqft
 
-        # Create input array for prediction
+        
         input_data = np.array([[area, bhk, bathroom, price_per_sqft]])
         
-        # Scale the input data
+        
         input_data_scaled = scaler.transform(input_data)
 
-        # Predict the price
+        
         prediction = model.predict(input_data_scaled)
 
         return render_template('index.html', data=int(prediction[0]))
